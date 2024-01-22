ea46a7ee4d8528248ed8e336de941229cf2faaf1
aokabi <aokabit@gmail.com>
2024-01-23 08:51:10 +0900

    Update scheduler configuration

--- tf/scheduler.tf
+++ tf/scheduler.tf
@@ -1,9 +1,17 @@
 resource "google_cloud_scheduler_job" "job" {
-  name     = "narou-notify-2"
+  name     = "narou-notify"
   description = "narou-notify cloud functionを定期実行する"
   region   = "asia-northeast1"
   time_zone = "Asia/Tokyo"
   schedule = "0 * * * *"
+
+  retry_config {
+    retry_count = 2
+    max_retry_duration = "60s"
+    min_backoff_duration = "5s"
+    max_backoff_duration = "3600s"
+    max_doublings = 5
+  }
 
   pubsub_target {
     topic_name = "projects/main-349812/topics/narou-notify"
